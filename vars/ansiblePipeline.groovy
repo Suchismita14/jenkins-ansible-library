c0f149047b33f8a2748bca79461e4002dd3baf65
Suchismita Parida <160097895+Suchismita14@users.noreply.github.com>
2024-12-08 20:41:00 +0530

    Update ansiblePipeline.groovy

--- vars/ansiblePipeline.groovy
+++ vars/ansiblePipeline.groovy
@@ -10,14 +10,18 @@ def call(Map config) {
             stage('User Approval') {
                 steps {
                     input message: config.approvalMessage, ok: config.approvalButton
+                    sh 'sudo chmod -R 400 /var/lib/jenkins/workspace/assignment-6/ohio.pem'
                 }
             }
             stage('Playbook Execution') {
                 steps {
                     script {
-                        ansiblePlaybook(
-                            playbook: config.playbookPath
-                        )
+                        withEnv(['ANSIBLE_HOST_KEY_CHECKING=False']) {
+                            ansiblePlaybook(
+                                playbook: config.playbookPath,
+                                inventory: 'inventory.ini'
+                            )
+                        }
                     }
                 }
             }
